2d865d08afc79797e4296ec4cd0b8953acf2be13
atom-rl <oscarfal2006@gmail.com>
2021-09-25 09:03:06 +0100

    comments

--- proton.js
+++ proton.js
@@ -1,6 +1,6 @@
-const { Intents } = require("discord.js");
-const dotenv = require("dotenv").config();
+const { Intents } = require("discord.js");//intents
+const dotenv = require("dotenv").config();//init .env
 const ProtonClient = require("./classes/ProtonClient");
-const Client = new ProtonClient({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
-Client.start(process.env.TOKEN);
+const Client = new ProtonClient({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]}); //Instansite client
+Client.start(process.env.TOKEN);//Login
 module.exports = Client;
